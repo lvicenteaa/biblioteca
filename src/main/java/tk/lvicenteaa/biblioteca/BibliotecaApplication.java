5bee64ffdb95c5f17c274405569d9b483ca62450
Luis Vicente Albarracin <albarracinluisvicente@gmail.com>
2023-01-03 13:58:16 -0500

    inicio de proyecto

---
+++ src/main/java/tk/lvicenteaa/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,13 @@
+package tk.lvicenteaa.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+}
